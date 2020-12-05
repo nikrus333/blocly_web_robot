886d13d10fe08d70be131827297d05dcd65dc475
nikrus333 <nikrus333@gmail.com>
2020-12-05 13:26:20 +0300

    first com

---
+++ server.py
@@ -0,0 +1,97 @@
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+class Message:
+    db = [
+        {'text':'Hi', 'author': 'I', 'time': time.time()},   #no global, it is diff all Classes S
+                {'text':'tt', 'author': 'kol', 'time': time.time()}
+    ]
+    def __init__(self):
+
+        self.db1 = [
+            {'text':'Hi', 'author': 'I', 'time': time.time()},
+            {'text':'tt', 'author': 'kol', 'time': time.time()}
+        ]
+    def send_message(self,text, author):
+        if isinstance(text,str) and isinstance(author,str):
+            # todo: time
+            self.db1.append({'text': text, 'author': author, 'time': time.time()})
+            print(text,author)
+            return "OK"
+        else:
+            return'Not OK'
+    def get_msgs(self):
+        print(self.db1)
+        return self.db1
+
+if __name__ == "__main__":
+    ms = Message()
+    ms1 = Message()
+    result = ms.send_message('Hello', 'Ivn')
+    result = ms1.send_message('H', 'Iaaaaaavn')
+    print(result)
+    ms.get_msgs()
+    ms1.get_msgs()
+    
+  """  
+    
+
+    
