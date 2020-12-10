3400964fcd593834789266badeff21b5e2c980bc
nikrus333 <nikrus333@gmail.com>
2020-12-10 15:18:39 +0300

    first commit

--- flask_run.py
+++ flask_run.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, send_from_directory
+from flask import render_template
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
-@app.route('/turtlebro_blockly/<path:path>')
-def send_js(path):
-    return send_from_directory('turtlebro_blockly', path)
 
+
+@app.route('/templates/<path:path>')
+def send_js(path):
+    return send_from_directory('templates', path)
+'''
+@app.route('/turtlebro_blockly')
+def send_js(name=None):
+    return send_from_directory('turtlebro_blockly', 'templates/hello.html')
+
+@app.route('/templates/<name>')
+def hello(name):
+    return render_template('hello.html', name=name)
+'''
+    
 if __name__ == "__main__":
     app.run()
