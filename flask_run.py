dd21bdbd385a1406d846f4894d967703db7617fc
nikrus333 <nikrus333@gmail.com>
2020-12-19 14:06:30 +0300

    second commit

--- flask_run.py
+++ flask_run.py
@@ -1,23 +1,25 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, url_for
 from flask import render_template
 
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
+menu = ["Настройка", "Видео", "blockly"]
 
+@app.route("/")
+def index():
+    print( url_for('index') )
+    return render_template('index.html', menu = menu)
 
+@app.route("/about")
+def about():
+    print( url_for('about') )
+    return render_template('about.html', title = "О сайте", menu = menu)
+    
 @app.route('/templates/<path:path>')
 def send_js(path):
     return send_from_directory('templates', path)
-'''
-@app.route('/turtlebro_blockly')
-def send_js(name=None):
-    return send_from_directory('turtlebro_blockly', 'templates/hello.html')
 
-@app.route('/templates/<name>')
-def hello(name):
-    return render_template('hello.html', name=name)
-'''
     
 if __name__ == "__main__":
     app.run()
